cd86ba52ad1137ec7ba3a2a17bfaf98bc20ea452
unknown <C:\Users\prog\AppData\Roaming\The Bat!>
2015-10-16 19:16:24 +0300

    зависимости

--- boot/passport.js
+++ boot/passport.js
@@ -1,8 +1,6 @@
 var config = require("nconf");
 var passport = require('passport');
 var AuthLocalStrategy = require('passport-local').Strategy;
-var AuthFacebookStrategy = require('passport-facebook').Strategy;
-var AuthVKStrategy = require('passport-vkontakte').Strategy;
 
 passport.use('local', new AuthLocalStrategy(
     function (username, password, done) {
@@ -17,49 +15,6 @@ passport.use('local', new AuthLocalStrategy(
 
         return done(null, false, {
             message: 'Неверный логин или пароль'
-        });
-    }
-));
-
-passport.use('facebook', new AuthFacebookStrategy({
-        clientID: config.get("auth:fb:app_id"),
-        clientSecret: config.get("auth:fb:secret"),
-        callbackURL: config.get("app:url") + "/auth/fb/callback",
-        profileFields: [
-            'id',
-            'displayName',
-            'profileUrl',
-           // "username",
-           // "link",
-            "gender",
-            "photos"
-        ]
-    },
-    function (accessToken, refreshToken, profile, done) {
-
-        //console.log("facebook auth: ", profile);
-
-        return done(null, {
-            username: profile.displayName,
-            photoUrl: profile.photos[0].value,
-            profileUrl: profile.profileUrl
-        });
-    }
-));
-
-passport.use('vk', new AuthVKStrategy({
-        clientID: config.get("auth:vk:app_id"),
-        clientSecret: config.get("auth:vk:secret"),
-        callbackURL: config.get("app:url") + "/auth/vk/callback"
-    },
-    function (accessToken, refreshToken, profile, done) {
-
-        //console.log("facebook auth: ", profile);
-
-        return done(null, {
-            username: profile.displayName,
-            photoUrl: profile.photos[0].value,
-            profileUrl: profile.profileUrl
         });
     }
 ));
