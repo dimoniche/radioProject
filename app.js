834e8c34b89d64d6619cf7d4624df367615ce8b5
unknown <C:\Users\prog\AppData\Roaming\The Bat!>
2015-10-29 23:07:51 +0300

    1

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var app = express();
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/radio');
+//var db = monk('localhost:27017/radio');
+var db = monk('dimoniche:lbvsx@ds045664.mongolab.com:45664/devices');
 
 config.argv()
     .env()
@@ -32,81 +33,81 @@ http.createServer(app).listen(app.get('port'), function () {
     }
 });
 
-var port_device = 3000;
-
-// запустим ожидание 
-var server = net.createServer(function (stream) {
-    console.log('server connected');
-
-    stream.on('data', function (data) {
-
-        console.log(data);
-
-        // разберем ответ
-        prepare_response(stream, data, port_device);
-
-    });
-
-    stream.on('close', function () {
-        console.log('server connection close');
-    });
-    stream.on('end', function () {
-        console.log('server disconnected');
-    });
-    stream.on('error', function () {
-        console.log('server error');
-    });
-});
-
-server.listen(port_device, function() {
-    console.log('server bound');
-});
-
-// запрос от прибора вида:
-//
-//	'deviceId'
-//  'device':
-//  'answer': {'id': "1",'name': "small device", 'state': 'on','lastTimeAccess': "12.01.2015"}
-//
-//
-// разбор пришедшего запроса от прибора
-function prepare_response(stream,data,port)
-{
-    // сначала разберем что приняли
-    var new_device = JSON.parse(data);
-    var collection = db.get('devices');
-
-    // большое устройство - сфоррмируем объект
-    var newDevice = {
-        'deviceId': new_device.deviceId,
-        'device': new_device.device,
-        'description': "Концентратор",
-        'lastAccessTime': new Date(),
-        'answer': new_device.answer,
-    }
-
-    collection.findOne({ 'deviceId': new_device.deviceId }).on('success', function (doc) {
-        
-        console.log('Устройство найдено');
-
-        // нашли уже такой - обновим статусы
-        collection.findAndModify(
-			{
-				"query": { "deviceId": new_device.deviceId },
-				"update": { "$set": { 
-					"answer": new_device.answer,
-					'lastAccessTime': new Date(),
-				}},
-				"options": { "new": true, "upsert": true }
-			});
-            
-            console.log('Устройство обновлено');
-            return;
-    });
-
-    // заносим в базу запись об новом устройстве
-    collection.insert(newDevice, function(err, result){
-
-    });
-
-}
+// var port_device = 3000;
+// 
+// // запустим ожидание 
+// var server = net.createServer(function (stream) {
+//     console.log('server connected');
+// 
+//     stream.on('data', function (data) {
+// 
+//         console.log(data);
+// 
+//         // разберем ответ
+//         prepare_response(stream, data, port_device);
+// 
+//     });
+// 
+//     stream.on('close', function () {
+//         console.log('server connection close');
+//     });
+//     stream.on('end', function () {
+//         console.log('server disconnected');
+//     });
+//     stream.on('error', function () {
+//         console.log('server error');
+//     });
+// });
+// 
+// server.listen(port_device, function() {
+//     console.log('server bound');
+// });
+// 
+// // запрос от прибора вида:
+// //
+// //	'deviceId'
+// //  'device':
+// //  'answer': {'id': "1",'name': "small device", 'state': 'on','lastTimeAccess': "12.01.2015"}
+// //
+// //
+// // разбор пришедшего запроса от прибора
+// function prepare_response(stream,data,port)
+// {
+//     // сначала разберем что приняли
+//     var new_device = JSON.parse(data);
+//     var collection = db.get('devices');
+// 
+//     // большое устройство - сфоррмируем объект
+//     var newDevice = {
+//         'deviceId': new_device.deviceId,
+//         'device': new_device.device,
+//         'description': "Концентратор",
+//         'lastAccessTime': new Date(),
+//         'answer': new_device.answer,
+//     }
+// 
+//     collection.findOne({ 'deviceId': new_device.deviceId }).on('success', function (doc) {
+//         
+//         console.log('Устройство найдено');
+// 
+//         // нашли уже такой - обновим статусы
+//         collection.findAndModify(
+// 			{
+// 				"query": { "deviceId": new_device.deviceId },
+// 				"update": { "$set": { 
+// 					"answer": new_device.answer,
+// 					'lastAccessTime': new Date(),
+// 				}},
+// 				"options": { "new": true, "upsert": true }
+// 			});
+//             
+//             console.log('Устройство обновлено');
+//             return;
+//     });
+// 
+//     // заносим в базу запись об новом устройстве
+//     collection.insert(newDevice, function(err, result){
+// 
+//     });
+// 
+// }
