3fce7bd09a7ae78ac1c9776368646aa0c51e9c09
dimoniche <dimoniche@gmail.com>
2015-11-11 00:47:55 +0300

    setup

--- setup.sh
+++ setup.sh
@@ -8,13 +8,7 @@ curl -sL https://deb.nodesource.com/setup | sudo bash -
 apt-get install -y nodejs
 apt-get install -y build-essential
 
-# ставим git
-apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
-apt-get install -y git
-
 # ставим проект
-git clone https://github.com/dimoniche/radioProject.git
-
 npm install node-gyp -g
 npm cache clean
 rm -rf node_modules
@@ -22,7 +16,8 @@ npm install
 
 # базу ставим
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | tee/etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
 apt-get install -y mongodb-org
 
 service mongod start
