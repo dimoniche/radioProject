3830214297b3301428a8369364c4a3b9d8ba21eb
dimoniche <dimoniche@gmail.com>
2015-11-11 00:50:11 +0300

    setup

--- setup.sh
+++ setup.sh
@@ -2,21 +2,21 @@
 
 apt-get update
 
-#ставим nodejs
+#пїЅпїЅпїЅпїЅпїЅпїЅ nodejs
 apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 apt-get install -y nodejs
 apt-get install -y build-essential
 
-# ставим проект
+# пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 npm install node-gyp -g
 npm cache clean
 rm -rf node_modules
 npm install
 
-# базу ставим
+# пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
 apt-get install -y mongodb-org
 
