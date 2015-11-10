ba4c59c09fbf783ee8c6700f83551504337ec66f
dimoniche <dimoniche@gmail.com>
2015-11-11 00:55:29 +0300

    setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 apt-get update
 
@@ -16,7 +16,7 @@ npm install
 
 # ���� ������
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
 apt-get install -y mongodb-org
 
